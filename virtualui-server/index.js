192972aefd20f888d11cd56265004302cd811a29
Uday Tomar <udaytomar972@gmail.com>
2026-07-21 00:14:12 +0530

    Update index.js

--- virtualui-server/index.js
+++ virtualui-server/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://virtualui-main-20t9.onrender.com",
     credentials:true
 }))
 
