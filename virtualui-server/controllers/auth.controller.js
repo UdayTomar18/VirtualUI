af3581f5a7a58b9dbe980e97066326bcaf1375f8
Uday Tomar <udaytomar972@gmail.com>
2026-07-20 23:54:50 +0530

    Update auth.controller.js

--- virtualui-server/controllers/auth.controller.js
+++ virtualui-server/controllers/auth.controller.js
@@ -12,9 +12,9 @@ export const googleAuth = async (req, res) => {
         }
         const token  = generateToken(user._id);
         res.cookie('token', token, {
-            httpOnly: true,
-            secure:false,
-            sameSite:'strict',
+            httpOnly: flase,
+            secure:true,
+            sameSite:'none',
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         })
         return res.status(200).json({user});
@@ -31,9 +31,9 @@ export const googleAuth = async (req, res) => {
 export const logOut = (req, res) => {
     try {
         res.clearCookie('token', {
-            httpOnly: true,
-            secure: false,
-            sameSite: 'strict',
+            httpOnly: false,
+            secure: true,
+            sameSite: 'none',
         });
         return res.status(200).json({ message: 'Logged out successfully' });
     } catch (error) {
